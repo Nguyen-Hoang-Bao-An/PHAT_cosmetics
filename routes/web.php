18f6ae9a96ce8183ca4a3815c0a43ca607a2803f
Nguyen-Hoang-Bao-An <97460281+Nguyen-Hoang-Bao-An@users.noreply.github.com>
2022-08-19 16:09:48 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\http\Controllers\CategoryController;
-use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\user\HomeController;
+use App\Http\Controllers\user\LoginController;
+use App\Http\Controllers\user\CartController;
+use App\Http\Controllers\user\CheckOutController;
+use App\Http\Controllers\user\ContactController;
+use App\Http\Controllers\user\BrandController;
+
 
 // For admin access
 Route::get('/admin', 'App\Http\Controllers\AdminController@loginAdmin');
@@ -13,272 +19,135 @@ Route::get('/home', function(){
     return view('home');
 });
 
-//Category
+
 Route::prefix('admin')->group(function(){
     Route::prefix('categories')->group(function () {
         Route::get('/', [
             'as' => 'categories.index',
             'uses' => 'App\Http\Controllers\CategoryController@index'
         ]);
-        
-    Route::get('/create', [
-        'as' => 'categories.create',
-        'uses' => 'App\Http\Controllers\CategoryController@create'
-    ]);
-    
-    Route::post('/store', [
-        'as' => 'categories.store',
-        'uses' => 'App\Http\Controllers\CategoryController@store'
-    ]);
 
-    Route::get('/edit/{id}', [
-        'as' => 'categories.edit',
-        'uses' => 'App\Http\Controllers\CategoryController@edit'
-    ]);
+        Route::get('/create', [
+            'as' => 'categories.create',
+            'uses' => 'App\Http\Controllers\CategoryController@create'
+        ]);
 
-    Route::post('/update/{id}', [
-        'as' => 'categories.update',
-        'uses' => 'App\Http\Controllers\CategoryController@update'
-    ]);
+        Route::post('/store', [
+            'as' => 'categories.store',
+            'uses' => 'App\Http\Controllers\CategoryController@store'
+        ]);
 
-    Route::get('/delete/{id}', [
-        'as' => 'categories.delete',
-        'uses' => 'App\Http\Controllers\CategoryController@delete'
-    ]);
+        Route::get('/edit/{id}', [
+            'as' => 'categories.edit',
+            'uses' => 'App\Http\Controllers\CategoryController@edit'
+        ]);
 
-});
+        Route::post('/update/{id}', [
+            'as' => 'categories.update',
+            'uses' => 'App\Http\Controllers\CategoryController@update'
+        ]);
 
-//Brand    
-Route::prefix('brands')->group(function () {
-    Route::get('/', [
-        'as' => 'brands.index',
-        'uses' => 'App\Http\Controllers\BrandController@index'
-    ]);
-    
-    Route::get('/create', [
-        'as' => 'brands.create',
-        'uses' => 'App\Http\Controllers\BrandController@create'
-    ]);
-    
-    Route::post('/store', [
-        'as' => 'brands.store',
-        'uses' => 'App\Http\Controllers\BrandController@store'
-    ]);
+        Route::get('/delete/{id}', [
+            'as' => 'categories.delete',
+            'uses' => 'App\Http\Controllers\CategoryController@delete'
+        ]);
 
-    Route::get('/edit/{id}', [
-        'as' => 'brands.edit',
-        'uses' => 'App\Http\Controllers\BrandController@edit'
-    ]);
+    });
 
-    Route::post('/update/{id}', [
-        'as' => 'brands.update',
-        'uses' => 'App\Http\Controllers\BrandController@update'
-    ]);
+    Route::prefix('brands')->group(function () {
+        Route::get('/', [
+            'as' => 'brands.index',
+            'uses' => 'App\Http\Controllers\BrandController@index'
+        ]);
 
-    Route::get('/delete/{id}', [
-        'as' => 'brands.delete',
-        'uses' => 'App\Http\Controllers\BrandController@delete'
-    ]);
+        Route::get('/create', [
+            'as' => 'brands.create',
+            'uses' => 'App\Http\Controllers\BrandController@create'
+        ]);
 
-});
+        Route::post('/store', [
+            'as' => 'brands.store',
+            'uses' => 'App\Http\Controllers\BrandController@store'
+        ]);
 
-//Menu
-Route::prefix('menus')->group(function () {
-    Route::get('/', [
-        'as' => 'menus.index',
-        'uses' => 'App\Http\Controllers\MenuController@index'
-    ]);
-    
-    Route::get('/create', [
-        'as' => 'menus.create',
-        'uses' => 'App\Http\Controllers\MenuController@create'
-    ]);
-    
-    Route::post('/store', [
-        'as' => 'menus.store',
-        'uses' => 'App\Http\Controllers\MenuController@store'
-    ]);
-    
-    Route::get('/edit/{id}', [
-        'as' => 'menus.edit',
-        'uses' => 'App\Http\Controllers\MenuController@edit'
-    ]);
+        Route::get('/edit/{id}', [
+            'as' => 'brands.edit',
+            'uses' => 'App\Http\Controllers\BrandController@edit'
+        ]);
 
-    Route::post('/update/{id}', [
-        'as' => 'menus.update',
-        'uses' => 'App\Http\Controllers\MenuController@update'
-    ]);
+        Route::post('/update/{id}', [
+            'as' => 'brands.update',
+            'uses' => 'App\Http\Controllers\BrandController@update'
+        ]);
 
-    Route::get('/delete/{id}', [
-        'as' => 'menus.delete',
-        'uses' => 'App\Http\Controllers\MenuController@delete'
-    ]);
+        Route::get('/delete/{id}', [
+            'as' => 'brands.delete',
+            'uses' => 'App\Http\Controllers\BrandController@delete'
+        ]);
 
-});
+    });
 
-//Product
-Route::prefix('products')->group(function () {
-    Route::get('/', [
-        'as' => 'products.index',
-        'uses' => 'App\Http\Controllers\AdminProductController@index'
-    ]);
-    
-    Route::get('/create', [
-        'as' => 'products.create',
-        'uses' => 'App\Http\Controllers\AdminProductController@create'
-    ]);
+    Route::prefix('menus')->group(function () {
+        Route::get('/', [
+            'as' => 'menus.index',
+            'uses' => 'App\Http\Controllers\MenuController@index'
+        ]);
 
-    Route::post('/store', [
-        'as' => 'products.store',
-        'uses' => 'App\Http\Controllers\AdminProductController@store'
-    ]);
+        Route::get('/create', [
+            'as' => 'menus.create',
+            'uses' => 'App\Http\Controllers\MenuController@create'
+        ]);
 
-    Route::get('/edit/{id}', [
-        'as' => 'products.edit',
-        'uses' => 'App\Http\Controllers\AdminProductController@edit'
-    ]);
+        Route::post('/store', [
+            'as' => 'menus.store',
+            'uses' => 'App\Http\Controllers\MenuController@store'
+        ]);
 
-    Route::post('/update/{id}', [
-        'as' => 'products.update',
-        'uses' => 'App\Http\Controllers\AdminProductController@update'
-    ]);
+        Route::get('/edit/{id}', [
+            'as' => 'menus.edit',
+            'uses' => 'App\Http\Controllers\MenuController@edit'
+        ]);
 
-    Route::get('/delete/{id}', [
-        'as' => 'products.delete',
-        'uses' => 'App\Http\Controllers\AdminProductController@delete'
-    ]);
-    
-});
+        Route::post('/update/{id}', [
+            'as' => 'menus.update',
+            'uses' => 'App\Http\Controllers\MenuController@update'
+        ]);
 
-//Slider
-Route::prefix('sliders')->group(function () {
-    Route::get('/', [
-        'as' => 'sliders.index',
-        'uses' => 'App\Http\Controllers\SliderAdminController@index'
-    ]);
+        Route::get('/delete/{id}', [
+            'as' => 'menus.delete',
+            'uses' => 'App\Http\Controllers\MenuController@delete'
+        ]);
 
-    Route::get('create', [
-        'as' => 'sliders.create',
-        'uses' => 'App\Http\Controllers\SliderAdminController@create'
-    ]);
+    });
 
-    Route::post('store', [
-        'as' => 'sliders.store',
-        'uses' => 'App\Http\Controllers\SliderAdminController@store'
-    ]);
+    Route::prefix('products')->group(function () {
+        Route::get('/', [
+            'as' => 'products.index',
+            'uses' => 'App\Http\Controllers\AdminProductController@index'
+        ]);
 
-    Route::get('edit/{id}', [
-        'as' => 'sliders.edit',
-        'uses' => 'App\Http\Controllers\SliderAdminController@edit'
-    ]);
+        Route::get('/create', [
+            'as' => 'products.create',
+            'uses' => 'App\Http\Controllers\AdminProductController@create'
+        ]);
 
-    Route::post('update/{id}', [
-        'as' => 'sliders.update',
-        'uses' => 'App\Http\Controllers\SliderAdminController@update'
-    ]);
-
-    Route::get('delete/{id}', [
-        'as' => 'sliders.delete',
-        'uses' => 'App\Http\Controllers\SliderAdminController@delete'
-    ]);
+        Route::post('/store', [
+            'as' => 'products.store',
+            'uses' => 'App\Http\Controllers\AdminProductController@store'
+        ]);
 
 
-});
-
-//Settings
-Route::prefix('settings')->group(function () {
-    Route::get('/', [
-        'as' => 'settings.index',
-        'uses' => 'App\Http\Controllers\AdminSettingsController@index'
-    ]);
-    
-    Route::get('/create', [
-        'as' => 'settings.create',
-        'uses' => 'App\Http\Controllers\AdminSettingsController@create'
-    ]);
-
-    Route::post('/store',[
-        'as' => 'settings.store',
-        'uses' => 'App\Http\Controllers\AdminSettingsController@store'
-    ]);
-
-    Route::get('/edit/{id}', [
-        'as' => 'settings.edit',
-        'uses' => 'App\Http\Controllers\AdminSettingsController@edit'
-    ]);
-
-    Route::post('/update/{id}',[
-        'as' =>'settings.update',
-        'uses' => 'App\Http\Controllers\AdminSettingsController@update'
-    ]); 
-
-    Route::get('/delete/{id}', [
-        'as' => 'settings.delete',
-        'uses' => 'App\Http\Controllers\AdminSettingsController@delete'
-    ]);
-});
-
-//User
-Route::prefix('users')->group(function () {
-    Route::get('/', [
-        'as' => 'users.index',
-        'uses' => 'App\Http\Controllers\AdminUserController@index'
-    ]);
-
-    Route::get('/create', [
-        'as' => 'users.create',
-        'uses' => 'App\Http\Controllers\AdminUserController@create'
-    ]);
-
-    Route::post('/store', [
-        'as' => 'users.store',
-        'uses' => 'App\Http\Controllers\AdminUserController@store'
-    ]);
-    
-    Route::get('/edit/{id}', [
-        'as' => 'users.edit',
-        'uses' => 'App\Http\Controllers\AdminUserController@edit'
-    ]);
-
-    Route::post('/update/{id}', [
-        'as' => 'users.update',
-        'uses' => 'App\Http\Controllers\AdminUserController@update'
-    ]);
-
-    Route::get('/delete/{id}', [
-        'as' => 'users.delete',
-        'uses' => 'App\Http\Controllers\AdminUserController@delete'
-    ]);
-    
-
-    
-
-    
-
-    
-
-    
-});
-
-//Roles
-Route::prefix('roles')->group(function () {
-    Route::get('/', [
-        'as' => 'roles.index',
-        'uses' => 'App\Http\Controllers\AdminRoleController@index'
-    ]);
-
-    Route::get('/create', [
-        'as' => 'roles.create',
-        'uses' => 'App\Http\Controllers\AdminRoleController@create'
-    ]);
+    });
 
 
-    
 
-    
 
-    
-});
+
+
+
+
+
+
 
 
 
@@ -296,6 +165,37 @@ Route::prefix('roles')->group(function () {
 //     return view('layouts.admin');
 // })
 
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::prefix('user')->name('user')->group(function(){
+
+    Route::get('home', [HomeController::class, 'home'])->name('home');
+
+    Route::get('login', [LoginController::class, 'login'])->name('login');
+
+    Route::post('postLogin', [LoginController::class, 'postLogin'])->name('postLogin');
+
+    Route::get('register', [LoginController::class, 'register'])->name('register');
+
+    Route::post('postRegister', [LoginController::class, 'postRegister'])->name('postRegister');
+
+    // Route::get('cart', [CartController::class, 'cart'])->name('cart');
+
+    Route::get('checkout', [CheckOutController::class, 'checkout'])->name('checkout');
+
+    Route::get('contact', [ContactController::class, 'contact'])->name('contact');
+
+    Route::get('brand', [BrandController::class, 'brand'])->name('brand');
+
+});
+
+//Login  google
+// Route::get('/login-google','GoogleController@login_google');
+// Route::get('/google/callback','GoogleController@callback_google');
+
+Route::get('/user/home','App\Http\Controllers\user\CartController@index');
+Route::get('/Add-Cart/{id}','App\Http\Controllers\user\CartController@AddCart');
+Route::get('/Delete-Item-Cart/{id}','App\Http\Controllers\user\CartController@DeleteItemCart');
+Route::get('/List-Carts','App\Http\Controllers\user\CartController@ViewListCart');
+Route::get('/Delete-Item-List-Cart/{id}','App\Http\Controllers\user\CartController@DeleteListItemCart');
